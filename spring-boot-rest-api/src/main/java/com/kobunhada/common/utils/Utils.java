c9e51a57902fc364e77de44404eaa6e06a7fc589
kobunhada <android.kobunhada@gmail.com>
2023-02-08 11:47:06 +0900

    Added comment.

--- spring-boot-rest-api/src/main/java/com/kobunhada/common/utils/Utils.java
+++ spring-boot-rest-api/src/main/java/com/kobunhada/common/utils/Utils.java
@@ -1,11 +1,18 @@
 package com.kobunhada.common.utils;
 
 public class Utils {
-    public static String replaceNullToDefaultValue(String replace, String defaultValue) {
-        if (replace == null ||  replace.trim().equals("")) {
+    /**
+     * If target is null or white-space or "", it replace default value.
+     *
+     * @param replaceTarget targetValue
+     * @param defaultValue defaultValue
+     * @return
+     */
+    public static String replaceNullToDefaultValue(String replaceTarget, String defaultValue) {
+        if (replaceTarget == null ||  replaceTarget.trim().equals("")) {
             return defaultValue;
         }
-        return replace;
+        return replaceTarget;
     }
 
 }
